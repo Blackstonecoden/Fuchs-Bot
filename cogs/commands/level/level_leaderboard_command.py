19ba60b5f5ba74b983e5224292a00284d134fede
blackstone12 <blackstone_12@outlook.de>
2024-08-16 21:04:39 +0200

    Using cache instead of fetching from the API

--- cogs/commands/level/level_leaderboard_command.py
+++ cogs/commands/level/level_leaderboard_command.py
@@ -23,10 +23,12 @@ class level_leaderboard_command(commands.Cog):
         user_list = ""
         pos = 1  
         for u in top_raw:
-            user: discord.User = await self.client.fetch_user(u[0])
-            top_users[u[0]] = [user.name, u[1]]
-            user_list += f"**#{pos}** • {user.name} • {int((u[1] / 50) ** (1 / 1.5))}\n"
-            await asyncio.sleep(0.1)
+            user: discord.User = self.client.get_user(u[0])
+            if user:
+                top_users[u[0]] = [user.name, u[1]]
+                user_list += f"**#{pos}** • {user.name} • {int((u[1] / 50) ** (1 / 1.5))}\n"
+            else:
+                user_list += f"**#{pos}** • `Null` • {int((u[1] / 50) ** (1 / 1.5))}\n"
             pos += 1
         embed = discord.Embed(title="Level Leaderboard", description=user_list, color=0xa7acb4)
         embed.set_thumbnail(url=interaction.guild.icon.url)
